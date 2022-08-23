b38939fc65ae49722774fe0b923ba9b5b8e41569
Adrian Basilio López <BL_980713@hotmail.com>
2022-08-23 01:23:49 -0500

    Implementación de tema global y correción de errores

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fl_components/router/app_routes.dart';
+import 'package:fl_components/theme/app_theme.dart';
 
 class HomeScreen extends StatelessWidget {
    
@@ -13,12 +14,10 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Componentes en Flutter'),
-        elevation: 0,
-        centerTitle: true,
       ),
       body: ListView.separated(
         itemBuilder: (context, i) => ListTile(
-          leading: Icon(menuOptions[i].icon, color: Colors.indigo),
+          leading: Icon(menuOptions[i].icon, color: AppTheme.primary),
           title: Text(menuOptions[i].name),
           onTap: () {
             //final route = MaterialPageRoute(
