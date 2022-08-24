e3b770afdfd42b9b4b705a52f43c164e17d5b1a9
Adrian Basilio López <BL_980713@hotmail.com>
2022-08-24 12:50:12 -0500

    Circle Avatar añadido

--- lib/screen/screens.dart
+++ lib/screen/screens.dart
@@ -1,4 +1,5 @@
 export 'package:fl_components/screen/alert_screen.dart';
+export 'package:fl_components/screen/avatar_screen.dart';
 export 'package:fl_components/screen/card_screen.dart';
 export 'package:fl_components/screen/home_screen.dart';
 export 'package:fl_components/screen/listview1_screen.dart';
