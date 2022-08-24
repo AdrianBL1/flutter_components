76e024f2d5a211f57e1c0a84d350d406b07b3cb2
Adrian Basilio López <BL_980713@hotmail.com>
2022-08-23 19:59:28 -0500

    Cambio del color primario

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static const Color primary = Colors.indigo;
+  static const Color primary = Colors.cyan;
   
   static final ThemeData lightTheme = ThemeData.light().copyWith(
     //Color Primario
